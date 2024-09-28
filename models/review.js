7e211c9d241afccc89b827a10225b80b6dbda5eb
hafidnrzs <hafidnurazis@gmail.com>
2024-09-29 02:31:19 +0700

    feat: add reviews permission

--- models/review.js
+++ models/review.js
@@ -3,7 +3,11 @@ const { Schema } = mongoose
 
 const reviewSchema = Schema({
     body: String,
-    rating: String
+    rating: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 })
 
 module.exports = mongoose.model('Review', reviewSchema)
