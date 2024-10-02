1475fd5c1fb08a1c68a3e0483ed0c91e0dbd1aba
hafidnrzs <hafidnurazis@gmail.com>
2024-10-02 20:18:39 +0700

    feat: add upload feature on edit campground page
    - also, refactor upload button in edit and new page with Bootstrap

--- controllers/campground.js
+++ controllers/campground.js
@@ -44,7 +44,10 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params
-    await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    campground.images.push(...imgs)
+    await campground.save()
     req.flash('success', 'Successfully edited a campground!')
     res.redirect(`/campgrounds/${id}`)
 }
