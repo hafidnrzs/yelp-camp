6e39b56483ea4819103b9ac393590276d3f976ae
Hafid Nur <hafidnurazis@gmail.com>
2025-01-20 16:59:06 +0700

    feat: update cluster map circle styles and increase seed data for campgrounds

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -29,26 +29,26 @@ map.on('load', () => {
         paint: {
             // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
             // with three steps to implement three types of circles:
-            //   * Blue, 20px circles when point count is less than 100
-            //   * Yellow, 30px circles when point count is between 100 and 750
-            //   * Pink, 40px circles when point count is greater than or equal to 750
+            //   * Cyan 500, 15px circles when point count is less than 100
+            //   * Blue 500, 20px circles when point count is between 100 and 750
+            //   * Indigo 500, 30px circles when point count is greater than or equal to 750
             'circle-color': [
                 'step',
                 ['get', 'point_count'],
-                '#51bbd6',
-                100,
-                '#f1f075',
-                750,
-                '#f28cb1'
+                '#00BCD4',
+                10,
+                '#2196F3',
+                30,
+                '#3F51B5'
             ],
             'circle-radius': [
                 'step',
                 ['get', 'point_count'],
+                15,
+                10,
                 20,
-                100,
                 30,
-                750,
-                40
+                25
             ]
         }
     });
