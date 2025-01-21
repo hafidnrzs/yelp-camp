5f3619f9103af7b33cc5afe6399833e916e0e38d
Hafid Nur <hafidnurazis@gmail.com>
2025-01-21 17:56:11 +0700

    feat(security): add mongo sanitize

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const ExpressError = require('./utils/ExpressError')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize');
 
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
@@ -35,6 +36,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize())
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
